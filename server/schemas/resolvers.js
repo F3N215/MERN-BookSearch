ed7194425dcab0bd41d73c40ac2c88508fae88a1
Daniel John <dcampo85@gmail.com>
2024-04-24 19:50:14 -0400

    resolvers.js updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,72 @@ const { signToken } = require("../utils/auth");
 const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
-    Query: {
-        me: aysnc (parent, args, context) => {
+  Query: {
+    profiles: async () => {
+      return User.find();
+    },
+  },
+
+  profile: async (parent, { profileId }) => {
+    return User.findOne({ _id: profileId });
+  },
+
+  me: async (parent, args, context) => {
+    if (context.user) {
+      return User.findOne({ _id: context.user._id });
     }
-}
+    throw new AuthenticationError("You're not logged in!");
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  users: async () => {
+    return User.find().select("-__v -password").populate("books");
+  },
+};
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,9 +5,10 @@ const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+  // track authentication errors
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
-      code: 'UNAUTHENTICATED',
+      code: "UNAUTHENTICATED",
     },
   }),
 
