d5c73fc821a1cc5ce44790b7d0d438cfc35dd78e
Daniel John <dcampo85@gmail.com>
2024-04-24 20:05:52 -0400

    wrap app.jsx using apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
+// create apollo provider component to make every request work with apollo server
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  createHttpLink 
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 import Navbar from './components/Navbar';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+      <ApolloClient client={client}>
       <Navbar />
       <Outlet />
+      </ApolloClient>
     </>
   );
 }
