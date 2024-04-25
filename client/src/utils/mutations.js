f5ecbe9e0dc68e385c303d2f2771d378f6b546cb
Daniel John <dcampo85@gmail.com>
2024-04-24 20:01:28 -0400

    update client mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from "@apollo/client";
+
+// executes the login mutation set up using the gql tag
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// executes the addUser mutation set up using the gql tag
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// executes the saveBook mutation set up using the gql tag
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+// executes the remoeBook mutation set up using the gql tag
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
