6ad8a41bcfdf817fcc3cdfc52a4ed88bee36eb1c
Daniel John <dcampo85@gmail.com>
2024-04-27 18:02:23 -0400

    update source

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 // create apollo provider component to make every request work with apollo server
 import { 
   ApolloClient, 
   InMemoryCache, 
   ApolloProvider, 
-  createHttpLink 
+  createHttpLink, 
 } from '@apollo/client';
-
 import { setContext } from '@apollo/client/link/context';
+import Navbar from './components/Navbar';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
-
-import Navbar from './components/Navbar';
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
